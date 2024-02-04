bae0a94e55a5a00576dcb0a23c0faa48e3d1f2cf
Eamon (WSL Laptop) <eamon.r.burns@gmail.com>
2024-02-04 00:28:56 -0800

    Add routes to add and list items in the "database"

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-    "net/http"
     "fmt"
     "log"
+    "net/http"
+    "strconv"
 
     "github.com/julienschmidt/httprouter"
 )
+
+type Item struct {
+    Name string
+    Value int
+}
+
+var db = []Item{}
+
+func main() {
+    router := httprouter.New()
+    router.GET("/", Index)
+    router.GET("/hello/:name", Hello)
+    router.GET("/add/:name/:value", AddItem)
+    router.GET("/list/", ListItems)
+
+    log.Print("Listening for requests...")
+
+    log.Fatal(http.ListenAndServe(":8000", router))
+}
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
     fmt.Fprint(w, "Welcome!\n")
@@ -16,10 +36,39 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     fmt.Fprintf(w, "Hello, %s!\n", ps.ByName("name"))
 }
 
-func main() {
-    router := httprouter.New()
-    router.GET("/", Index)
-    router.GET("/hello/:name", Hello)
+func AddItem(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    name := ps.ByName("name")
+    value, err := strconv.Atoi(ps.ByName("value"))
+    if err != nil {
+        fmt.Fprintf(w, "Error, `%s` is not a valid integer", ps.ByName("value"))
+        return
+    }
+    fmt.Fprintf(w,
+        "Adding item:\n\tName: %s\n\tValue: %d",
+        name,
+        value,
+    )
 
-    log.Fatal(http.ListenAndServe(":8000", router))
+    item := Item{ Name: name, Value: value }
+
+    db = append(db, item)
+    log.Printf("Adding item: %v", item)
 }
+
+func ListItems(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    if len(db) == 0 {
+        log.Print("Not listing items")
+        fmt.Fprint(w, "No items, add them by going to `/add/<name>/<value>`")
+        return
+    }
+    log.Print("Listing items")
+    for i, item := range db {
+        fmt.Fprintf(w,
+            "Item %d:\n\tName: %s\n\tValue: %d\n\n",
+            i,
+            item.Name,
+            item.Value,
+        )
+    }
+}
+
